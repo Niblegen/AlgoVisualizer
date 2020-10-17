f1602b89b9868e3cb595b000bee87d995cdd040d
Judaya, John Ray <judaya93@gmail.comm>
2020-10-17 20:53:30 +0800

    Passing value doesn't work I dunno how

--- src/index.js
+++ src/index.js
@@ -2,19 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+class AlgoVisual extends React.Component {
+  constructor(props) {
+    super(props);
+    this.value = this.props.value;
+    this.testVarible= "this is a test";
+  }
+    // let value = {props.value};
+
+  render(){
+    return(
+      <div>
+        {this.props.value}
+        qweqweqweqw
+      </div>
+      
+    );
+  }
+}
+
 class LinearSearch extends React.Component {
   constructor(props) {
-    super();
-    this.state = {
-      
-    };
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    return (
+      <AlgoVisual
+        value = {this.state.value}
+        
+      />
+    );
+  }
+
   render() {
     return (
-      <div className="algo">
-
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
