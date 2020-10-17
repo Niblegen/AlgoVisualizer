4ce35a5e5ab0dccdb15ed114789c2612a7df3d07
Judaya, John Ray <judaya93@gmail.comm>
2020-10-17 19:59:17 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class LinearSearch extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      
+    };
+  }
+  
+  render() {
+    return (
+      <div className="algo">
+
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <LinearSearch />,
+  document.getElementById('root')
+);
+
+// ========================================
